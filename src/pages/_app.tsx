c6261511bce955446fc07abeefd5b8bc08d27d4d
Anurag30112003 <anuragsharma011011@gmail.com>
2023-04-11 22:50:02 +0530

    added analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
+ 
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
+ 
+export default MyApp;
