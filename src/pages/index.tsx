e15255d3881fa215aaa3450fcd7fa98420ae96a6
Anurag30112003 <anuragsharma011011@gmail.com>
2023-04-13 21:46:34 +0530

    auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,21 @@
 // import Image from 'next/image'
 // import styles from '../styles/Home.module.css'
+import { SignInButton,SignOutButton } from "@clerk/nextjs";
+import { useAuth } from "@clerk/nextjs";
 
 export default function Home() {
-  return (
-    <main className="text-4xl flex justify-center mt-[40vh]">
-      <div className="font-semibold">Hello World from GPTforms.</div>
-    </main>
-  );
+  const { isLoaded, userId, sessionId, getToken } = useAuth();
+
+  if (!isLoaded) return <div>Loading...</div>;
+  else if (!userId) {
+    return <SignInButton />;
+  } else {
+    return ( <>
+      <main className="text-4xl flex justify-center mt-[40vh]">
+        <div className="font-semibold">Hello World from GPTforms.</div>
+        <SignOutButton/>
+      </main>
+    </>
+    );
+  }
 }
