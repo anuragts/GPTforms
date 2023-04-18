5ad8069ebf7234d5d6f15791c33a692ea9dba5c7
Anurag30112003 <anuragsharma011011@gmail.com>
2023-04-18 09:48:17 +0530

    create multiple fields successfull

--- src/pages/form/[id].tsx
+++ src/pages/form/[id].tsx
@@ -1,29 +1,57 @@
 import { useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/router";
-import { ToastContainer, toast } from "react-toastify";
+import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-export default function Form() {
+interface FormProps {
+  formId: string;
+}
+
+interface Field {
+  name: string;
+  description: string;
+}
+
+export default function FieldForm({ formId }: FormProps) {
   const router = useRouter();
-  const { id }  = router.query ;
-  const [name, setName] = useState<string>("");
-  const [description, setDescription] = useState<string>("");
-  const [loading, setLoading] = useState<boolean>(false);
+  const { id } = router.query;
+  const [fields, setFields] = useState<Field[]>([
+    { name: "", description: "" },
+  ]);
+  const [loading, setLoading] = useState(false);
+
+  const handleAddField = () => {
+    setFields([...fields, { name: "", description: "" }]);
+  };
+
+  const handleFieldChange = (
+    index: number,
+    key: keyof Field,
+    value: string
+  ) => {
+    setFields((prevFields) =>
+      prevFields.map((field, i) =>
+        i === index ? { ...field, [key]: value } : field
+      )
+    );
+  };
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setLoading(true);
 
     try {
-      const response = await axios.post("/api/Form/createFields", {
-        name,
-        description,
-        form_id: id,
-      });
-
-      setName("");
-      setDescription("");
+      await Promise.all(
+        fields.map((field) =>
+          axios.post("/api/Form/createFields", {
+            name: field.name,
+            description: field.description,
+            form_id: id,
+          })
+        )
+      );
+      setFields([{ name: "", description: "" }]);
       toast.success("Field created successfully!");
     } catch (error) {
       console.error(error);
@@ -35,29 +63,37 @@ export default function Form() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor="name">Name:</label>
-      <input
-        id="name"
-        name="name"
-        type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        required
-      />
+      {fields.map((field, index) => (
+        <div key={index}>
+          <label htmlFor={`name-${index}`}>Name:</label>
+          <input
+            id={`name-${index}`}
+            name={`name-${index}`}
+            type="text"
+            value={field.name}
+            onChange={(e) => handleFieldChange(index, "name", e.target.value)}
+            required
+          />
 
-      <label htmlFor="description">Description:</label>
-      <textarea
-        id="description"
-        name="description"
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-        required
-      />
+          <label htmlFor={`description-${index}`}>Description:</label>
+          <textarea
+            id={`description-${index}`}
+            name={`description-${index}`}
+            value={field.description}
+            onChange={(e) =>
+              handleFieldChange(index, "description", e.target.value)
+            }
+          />
+        </div>
+      ))}
 
       <button type="submit" disabled={loading}>
-        {loading ? "Loading..." : "Create Field"}
+        {loading ? "Loading..." : "Create Fields"}
       </button>
-      <ToastContainer />
+
+      <button type="button" onClick={handleAddField}>
+        +
+      </button>
     </form>
   );
 }
