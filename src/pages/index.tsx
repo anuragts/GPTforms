d85828e64d46c9483cf4d27b97d057a36bea1adf
Anurag30112003 <anuragsharma011011@gmail.com>
2023-04-21 23:49:23 +0530

    something

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import { SignInButton, SignOutButton } from "@clerk/nextjs";
 import { useAuth, useUser } from "@clerk/nextjs";
+import { SignInButton } from "@clerk/nextjs";
 import { useEffect } from "react";
 import axios from "axios";
 import Link from "next/link";
@@ -39,7 +39,11 @@ export default function Home() {
       <main className="flex flex-col items-center justify-center h-screen bg-gray-100">
         <div className="text-center">
           <h1 className="text-4xl font-bold mb-4">Hello World from GPTforms</h1>
-          <Link href={"/form/create"}>Try Now.</Link>
+          {userId ? (
+            <Link href={"/form/create"}>Try Now.</Link>
+          ) : (
+            <SignInButton />
+          )}
         </div>
       </main>
     );
