40ddd0ab78d2b0380e6d22a2e870eaaadeb3d0f7
Abhijit Roy <roya51788@gmail.com>
2023-04-24 14:05:35 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,7 +38,8 @@ export default function Home() {
     return (
       <main className="flex flex-col items-center justify-center h-screen bg-gray-100">
         <div className="text-center">
-          <h1 className="text-4xl font-bold mb-4">Forms with touch of AI</h1>
+          <h1 className="text-4xl font-bold mb-4"> Summarize Forms With <br />
+          <span className="orange_gradient">GPTforms</span></h1>
           {userId ? (
             <div className="bg-blue-500 text-white font-bold py-2 px-4 rounded">
             <Link href={"/form/create"}>Try Now.</Link>
