f1628af7a0b267f0a018f15670d936a4f779cdc4
Anurag30112003 <anuragsharma011011@gmail.com>
2023-04-24 11:15:50 +0530

    next update

---
+++ src/pages/form/createAI.tsx
@@ -0,0 +1,10 @@
+import { useState,useEffect } from "react";
+import { useAuth } from "@clerk/nextjs";
+import axios from "axios";
+
+
+export default function createAI() {
+  return (
+    <div>createAI</div>
+  )
+}
